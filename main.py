a654966c3be596aa5da8fbf9cfc68a2b647d552c
Jesse Smith <jsmith@resonatingmedia.com>
2025-05-10 14:46:53 -0300

    Initial commit.

---
+++ main.py
@@ -0,0 +1,432 @@
+import machine
+import network
+import socket
+import sys
+import _thread
+import time
+from machine import Pin
+from robot import Robot
+
+
+# Network credentials
+default_port = 40801
+ssid = "Pineapple"
+password = "Winter2024"
+
+# The Pico board's LED
+led = Pin("LED", Pin.OUT)
+# Are we flashing the LED?
+pico_blinking = 0
+
+robot = Robot()
+
+
+def enable_networking(wait_time):
+  attempts = 0
+  time.sleep(wait_time)
+  wlan = network.WLAN(network.STA_IF)
+  wlan.active(True)
+  wlan.connect(ssid, password)
+  while wlan.status() != 3 and attempts < 10:
+     time.sleep(1)
+     attempts += 1
+
+  if wlan.status() == 3:
+     address = wlan.ifconfig()
+     return address[0]
+  else:
+     
+     return False
+
+
+
+def help():
+   send_string = "Tasks the Pico knows how to do:\n\n"
+   send_string += "blink [times] - toggle LED on/off <times> or enable/disable if not number specified\n"
+   send_string += "echo [text] - repeats text back to client\n"
+   send_string += "hello - say Hello to the client\n"
+   send_string += "help - show this list of commands\n"
+   send_string += "light <on/off> - turn the LED on or off\n"
+   send_string += "sleep <seconds> - wait\n"
+   send_string += "temp - try to sense temperature (somewhat inaccurate)\n"
+   send_string += "exit - disconnect client\n\n"
+   
+   send_string += "Tasks the robot knows how to do:\n\n"
+   send_string += "distance - distance to nearest object in cm\n"
+   send_string += "forward - move the buggy forward until it reaches a wall.\n"
+   send_string += "halt - come to a complete stop\n"
+   send_string += "honk - beep the horn\n"
+   send_string += "lights <colour> - change the colour of the LED lights on the buggy\n"
+   send_string += "reverse - move the buggy backwards\n"
+   send_string += "speed [new_speed] - get the current speed or set engines to a new speed\n"
+   send_string += "spin <left/right> - spin the buggy in place\n"
+   send_string += "status - get status report from the buggy\n"
+   send_string += "turn <degrees> - turn the buggy left or right a number of degrees\n"
+   
+   send_string += "\n"
+   return send_string
+    
+
+def do_blinking():
+   global pico_blinking
+
+   # Blink once for each number above zero
+   if pico_blinking > 0:
+      led.toggle()
+      pico_blinking -= 1
+   # Blink endlessly if below zero
+   elif pico_blinking < 0:
+       led.toggle()
+
+
+
+def Reset_Everything():
+    global pico_blinking
+    global robot
+    
+    pico_blinking = 0
+    led.value(1)     # Indicate we are ready for a new connection
+    robot.lights_off()
+    robot.halt()
+    
+
+def Update_Everything():
+    global robot
+    
+    while True:
+        do_blinking()
+        robot.update()
+        time.sleep(1)
+    
+    
+    
+def blink(command_line, socket):
+    global pico_blinking
+   
+    if len(command_line) >= 2:
+        try:
+            blink_times = int(command_line[1])
+            send_string = "Blinking " + str(blink_times) + " times.\n"
+            pico_blinking = blink_times
+        except:
+            send_string = "I did not understand " + command_line[1] + "\n"
+
+    else:
+        # We did not get a number
+        if pico_blinking == 0:
+            pico_blinking = -1
+            send_string = "Blinking is now enabled.\n"
+        else:
+            pico_blinking = 0
+            send_string = "Blinking is now disabled.\n"
+            
+    return send_string
+
+ 
+
+def go_to_sleep(command_line, socket):
+    sleep_time = 1.0
+    if len(command_line) >= 2:
+        try:
+           sleep_time = float(command_line[1])
+        except:
+           send_string = "I did not understand " + command_line[1] + "\n"
+           socket.send( send_string.encode() )
+
+    send_string = "Sleeping for " + str(sleep_time) + " seconds.\n"
+    socket.send( send_string.encode() )
+    time.sleep(sleep_time)
+    send_string = "Waking.\n"
+    return send_string
+
+
+def light_on_off(command_line):
+   if len(command_line) >= 2:
+      if command_line[1] == "on":
+         led.value(1)
+         return_string = "Turned on LED.\n"
+      elif command_line[1] == "off":
+         led.value(0)
+         return_string = "Turned off LED.\n"
+      else:
+         return_string = "Command " + command_line[1] + " not recognized.\n"
+   else:
+      return_string = "Turn light on or off?\n"
+   return return_string
+
+
+
+def change_lights(command_line):
+    global robot
+    colour = robot.buggy.WHITE
+    
+    if len(command_line) < 2:
+        return_string = "Please provide the light colour, such as red, yellow, green, blue, or purple.\n"
+        return return_string
+    if command_line[1] == "red":
+        colour = robot.buggy.RED
+    elif command_line[1] == "yellow":
+        colour = robot.buggy.YELLOW
+    elif command_line[1] == "green":
+        colour = robot.buggy.GREEN
+    elif command_line[1] == "blue":
+        colour = robot.buggy.BLUE
+    elif command_line[1] == "purple":
+        colour = robot.buggy.PURPLE
+    elif command_line[1] == "off":
+        return_string = "Turning off buggy lights.\n"
+        robot.lights_off()
+        return return_string
+    else:
+        return_string = "I did not understand the colour " + command_line[1] + "\n"
+        return return_string
+    return_string = "Setting the lights to " + command_line[1] + "\n"
+    robot.set_lights([0,1,2,3], colour)
+    return return_string
+
+
+
+def sense_temperature():
+    sensor_temp = machine.ADC(4)
+    conversion_factor = 3.3 / (65535)
+    try:
+       reading = sensor_temp.read_u16() * conversion_factor
+    except:
+       send_string = "Unable to read temperature sensor.\n"
+       return send_string
+    
+    try:
+       temperature = round(27 - (reading - 0.706)/0.001721, 2)
+       temperature -= 17.0
+       send_string = "Current temperature: " + str(temperature) + "C\n"
+    except:
+        send_string = "Unable to convert temperature.\n"
+    return send_string
+
+
+def get_distance():
+    global robot
+    distance_in_cm = robot.get_forward_distance()
+    send_string = "Distance to nearest object is " + str(distance_in_cm) + ".\n"
+    return send_string
+
+
+def spin_buggy(command_line):
+    global robot
+    direction = "r"
+    send_string = "Spinning buggy to the right.\n" 
+    
+    if len(command_line) >= 2:
+        if command_line[1] == "left":
+            direction = "l"
+            send_string = "Spinning buggy to the left.\n"
+    
+    robot.spin(direction)
+    return send_string
+
+
+def turn_buggy(command_line):
+    global robot
+    if len(command_line) < 2:
+        send_string = "Please specify how many degrees to turn. Negative degrees for left.\n"
+        return send_string
+
+    if not command_line[1].isnumeric():
+        send_string = "I did not understand " + command_line[1] + ". Please use a number.\n"
+        return send_string
+
+    degrees = int(command_line[1])
+    status = robot.turn(degrees)
+    if status:
+       send_string = "Turning buggy " + str(degrees) + ".\n"
+    else:
+       send_string = "The buggy ran into a problem trying to turn.\n"
+    return send_string
+
+
+
+def halt_buggy():
+    global robot
+    robot.halt()
+    send_string = "Coming to a stop.\n"
+    return send_string
+
+
+def move_forward():
+    global robot
+    status = robot.forward()
+    if status:
+        send_string = "Moving forward.\n"
+    else:
+        send_string = "Cannot move forward, something is in the way.\n"
+    return send_string
+
+def move_reverse():
+    global robot
+    status = robot.reverse()
+    if status:
+        send_string = "Moving in reverse.\n"
+    else:
+        send_string = "Cannot move in reverse.\n"
+    return send_string
+
+
+
+def set_speed(command_line):
+    global robot
+    if len(command_line) < 2:
+        old_speed = robot.get_speed()
+        send_string = "Current speed: " + str(old_speed) + "\n"
+        return send_string
+    new_speed = int(command_line[1])
+    if new_speed < 0 or new_speed > 100:
+        send_string = "Speed needs to be in the range of 0-100\n"
+        return send_string
+    robot.set_speed(new_speed)
+    send_string = "Set new speed to " + str(new_speed) + "\n"
+    return send_string
+
+
+def get_status():
+    global robot
+    send_string = "Status report from buggy...\n"
+    send_string += "Speed: " + str(robot.speed) + "\n"
+    send_string += "Left engine: "
+    if robot.left_motor == 0:
+        send_string += "off\n"
+    elif robot.left_motor < 0:
+        send_string += "reverse\n"
+    else:
+        send_string += "forward\n"
+    send_string += "Right engine: "
+    if robot.right_motor == 0:
+        send_string += "off\n"
+    elif robot.right_motor < 0:
+        send_string += "reverse\n"
+    else:
+        send_string += "forward\n"
+    send_string += "Distance to nearest object: " + str(robot.forward_distance) + "cm\n"
+    return send_string
+
+
+def parse_incoming_command(command, client_socket):
+    global robot
+    command_and_args = command.split()
+    return_value = True
+    args_length = len(command_and_args)
+
+    if args_length >= 1:
+      cmd = command_and_args[0].lower()
+
+    if args_length < 1:
+       send_string = "Nothing received\n"
+    elif cmd == "distance":
+        send_string = get_distance()
+    elif cmd == "echo":
+       send_string = command + "\n"
+    elif cmd == "exit":
+       send_string = "Good-bye\n"
+       return_value = False
+    elif cmd == "forward":
+        send_string = move_forward()
+    elif cmd == "halt":
+        send_string = halt_buggy()
+    elif cmd == "hello":
+       send_string = "Hello\n"
+    elif cmd == "help":
+        send_string = help()
+    elif cmd == "honk":
+        robot.honk()
+        send_string = "Beep beep\n"
+    elif cmd == "blink":
+        send_string = blink(command_and_args, client_socket)
+    elif cmd == "light":
+        send_string = light_on_off(command_and_args)
+    elif cmd == "lights":
+        send_string = change_lights(command_and_args)
+    elif cmd == "reverse":
+        send_string = move_reverse()
+    elif cmd == "sleep":
+       send_string = go_to_sleep(command_and_args, client_socket)
+    elif cmd == "speed":
+        send_string = set_speed(command_and_args)
+    elif cmd == "spin":
+        send_string = spin_buggy(command_and_args)
+    elif cmd == "status":
+        send_string = get_status()
+    elif cmd == "temp":
+        send_string = sense_temperature()
+    elif cmd == "turn":
+        send_string = turn_buggy(command_and_args)
+    else:
+       send_string = "Command not recognized.\n"
+
+    client_socket.send( send_string.encode() )
+    return return_value
+
+
+
+def create_network_service(host='0.0.0.0', port=default_port):
+    server_running = True
+    
+    # Create a socket object
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    # Bind the socket to the host and port
+    server_socket.bind( (host, port) )
+
+    # Listen for incoming connections
+    server_socket.listen(5)
+    print(f"Server listening on {host}:{port}")
+
+    _thread.start_new_thread(Update_Everything, ())
+    while server_running:
+        # Accept new connections
+        client_socket, address = server_socket.accept()
+        print(f"Connected to {address}")
+        client_socket.send( "Type 'help' to get a list of recognized commands.\n".encode() )
+
+        Reset_Everything()
+        
+        # Receive data from the client
+        keep_running = True
+        while keep_running:
+            client_socket.send( "Ready> ".encode() )
+            data = client_socket.recv(1024)
+            if data:
+               print(f"Received: {data.decode()}")
+               status = parse_incoming_command(data.decode(), client_socket)
+               keep_running = status
+            else:
+               keep_running = False
+
+        # Close the client socket
+        Reset_Everything()
+        client_socket.close()
+        
+
+
+def signal_handler(my_signal, temp):
+   sys.exit(0)
+
+
+def main():
+    
+    # Init pico
+    delay = 1
+    my_address = enable_networking(delay)
+    if my_address:
+       print("My IP address ", my_address)
+       led.value(1)
+       # Set up listening socket and parse commands
+       create_network_service(my_address, default_port)
+
+    else:
+       print("Unable to connect to network.")
+       led.value(0)
+
+    
+
+if __name__ == "__main__":
+   main()
+
---
+++ robot.py
@@ -0,0 +1,215 @@
+import PicoAutonomousRobotics
+import time
+
+# User facing functions
+# reset - reset the robot's position and direction to (0,0) and 0 degrees and turn off lights
+# halt  - stop everything
+# update - check for objects in front or behind us, update coorindates
+#          any other maintenance like flashing lights
+# get_coordinates - return current (x,y)
+# set_coordinates - set (x,y)
+# get_speed - get current motor speed
+# set_speed - set motor speed (0-100) - does not engage motor, but sets how fast it will go
+# get_direction - compass direction estimate
+# set_direction - set direction 0-359
+# honk - beep the horn
+# get_forward_distance - cm to anything in front of us
+# get_backward_distance - cm to anything behind us
+# forward - move forward at set speed until told to stop or we get too close to something
+# spin - spin in place to the left ("l") or right ("r")
+# turn - turn left or right a specified number of degrees
+
+
+# Constants
+DEFAULT_SPEED = 50
+TOO_CLOSE = 30
+MIDDLE_DISTANCE = 60
+LIGHT_LEVEL = 15
+
+ 
+class Robot:
+
+   def __init__(self):
+       # Init the buggy, make sure it is stopped, quiet, and dark
+       self.buggy = PicoAutonomousRobotics.KitronikPicoRobotBuggy()
+       self.halt() 
+       self.reset()
+       self.buggy.setMeasurementsTo("cm")
+
+
+   def reset(self):
+       self.x = 0
+       self.y = 0
+       self.direction = 0
+       self.get_forward_distance()
+       self.set_lights([0,1,2,3], self.buggy.GREEN)
+       self.buggy.setBrightness(LIGHT_LEVEL)
+       self.buggy.show()
+       self.buggy.silence()
+       self.speed = 0
+     
+
+   def halt(self):
+       self.buggy.motorOff("l")
+       self.buggy.motorOff("r")
+       self.left_motor = 0
+       self.right_motor = 0
+
+
+
+
+   def update(self):
+       # Logic is reversed to put sensor on rear
+       self.forward_distance = self.buggy.getDistance("r")
+       if self.forward_distance <= TOO_CLOSE and self.forward_distance > 1:
+           self.set_lights([0,1,2,3], self.buggy.RED)
+           # if we are moving forward, check for objects
+           if self.left_motor > 0 and self.right_motor > 0:
+               self.halt()
+       elif self.forward_distance > TOO_CLOSE and self.forward_distance < MIDDLE_DISTANCE:
+              self.set_lights([0,1,2,3], self.buggy.BLUE)
+       else:
+          self.set_lights([0,1,2,3], self.buggy.GREEN)
+             
+
+
+   def set_lights(self, light_array, colour):
+       for light in light_array:
+           if light >= 0 and light <= 3:
+               self.buggy.setLED(light, colour)
+               
+       self.buggy.show()
+
+   def lights_off(self):
+       for light in range(4):
+           self.buggy.clear(light)
+       self.buggy.show()
+       
+       
+       
+   def get_direction(self):
+       return self.direction
+
+
+   def set_direction(self, new_direction):
+       if new_direction >= 0 and new_direction < 360:
+           self.direction = new_direction
+           return True
+       return False
+
+
+   def get_coordinates(self):
+       position = (self.x, self.y)       
+       return position
+
+
+   def set_coordinates(self, x, y):
+       self.x = x
+       self.y = y
+       return True
+
+
+   def get_speed(self):
+       return self.speed
+
+
+   def set_speed(self, new_speed):
+      if new_speed >= 0 and new_speed <= 100:
+           self.speed = new_speed
+           # if motors are running, engage them at new speed
+           # The directions are all reversed to handle broken motor
+           if self.left_motor < 0:
+               self.buggy.motorOn("l", "f", self.speed)
+           elif self.left_motor > 0:
+               self.buggy.motorOn("l", "r", self.speed)
+           else:
+               self.buggy.motorOff("l")
+           if self.right_motor < 0:
+               self.buggy.motorOn("r", "f", self.speed)
+           elif self.left_motor > 0:
+               self.buggy.motorOn("r", "r", self.speed)
+           else:
+               self.buggy.motorOff("r")
+           return True
+      return False
+
+
+   def honk(self):
+      self.buggy.beepHorn()
+
+
+   def get_forward_distance(self):
+       # Logic is reversed to read sensor on rear
+       self.forward_distance = self.buggy.getDistance("r")
+       return self.forward_distance
+
+
+
+
+   def forward(self):
+       # First, stop what we are doing
+       self.halt()
+       # check there is nothing in front of us
+       distance = self.forward_distance
+       if distance > TOO_CLOSE or distance < 0:
+           # Check if we need to engage throttle
+           if self.speed <= 0:
+              self.set_speed(DEFAULT_SPEED)
+              
+           self.left_motor = 1
+           self.right_motor = 1
+           # Logic is reversed to handle broken motor
+           self.buggy.motorOn("r", "r", self.speed)
+           self.buggy.motorOn("l", "r", self.speed)
+           return True
+       else:    # we are too close to things in front, stop
+          return False
+
+
+   def reverse(self):
+       self.halt()
+       if self.speed <= 0:
+           self.set_speed(DEFAULT_SPEED)
+       
+       self.left_motor = -1
+       self.right_motor = -1
+       # Logic is reversed to handle broken motor
+       self.buggy.motorOn("r", "f", self.speed)
+       self.buggy.motorOn("l", "f", self.speed)
+       return True
+    
+
+   def spin(self, left_right):
+       # Stop before we do the next action
+       self.halt()
+       # Spin in place
+       if self.speed <= 0:
+           self.set_speed(DEFAULT_SPEED) 
+
+       if left_right == "r":   # spin right
+          self.buggy.motorOn("r", "r", self.speed)
+          # self.buggy.motorOn("r", "f", self.speed)
+       else:    # spin left
+          self.buggy.motorOn("l", "r", self.speed)
+
+
+   # Work out how long it will take us to turn
+   # N degrees. Then decide if we want to turn left or right.
+   # Use the "spin" function to get us moving, then sleep
+   # while we turn, then stop the engines.
+   def turn(self, degrees):
+       self.halt()
+       if degrees > 359 or degrees < -359:
+          return False
+       # Estimated time it will take to turn us in a circle
+       degrees_per_second = 360
+       degrees_to_turn = abs(degrees)
+       sleep_time = degrees_to_turn / degrees_per_second
+       if degrees < 0:
+          self.spin("l")
+       else:
+          self.spin('r")
+       time.sleep(sleep_time)
+       self.halt()
+       return True
+
